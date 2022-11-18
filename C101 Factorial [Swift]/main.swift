ae98d40c3426a6f11dcb2d8253b72ecdf2c653eb
Jake Podewils <jake.podewils@student.allenisd.org>
2022-11-18 16:27:25 -0600

    factorial

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,25 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+func factorial(fNumber: Int)-> Int {
+    if n <= 1 {
+        print("Calculating factorial(\(n)).")
+        print("Terminal case, returning 1.")
+        print("The final result is 1.")
+        return 1
+    } else {
+        print("Calculating factorial(\(n)).")
+        let calc = n * n-1
+        factorial(fNumber: n-1)
+        print("Terminal case, returning 1.")
+        print("Non-Terminal case, returning \(calc)")
+    }
+}
+factorial(fNumber: n-1)
